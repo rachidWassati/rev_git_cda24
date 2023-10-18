b0fbfd8f9797d9b254cb14e0fdc094a31e28f0d6
rachidWassati <rachid.jeffali@wassati.com>
2023-10-18 10:34:39 +0200

    pas besoin d'etre aussi precis sur PI

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const calculArea = (rayon) => Math.PI * rayon ** 2
+const calculArea = (rayon) => 3.14 * rayon ** 2
 
-const calculVolume = (rayon) => 4 / 3 * (Math.PI * rayon ** 3)
+const calculVolume = (rayon) => 4 / 3 * (3.14 * rayon ** 3)
