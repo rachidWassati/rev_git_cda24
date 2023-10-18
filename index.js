37848844dbe8bc257d9a478263642d8e6e692c2b
rachidWassati <rachid.jeffali@wassati.com>
2023-10-18 10:53:39 +0200

    fixes #1, ajout de Math.PI pour plus de precision

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const calculArea = (rayon) => 3.14 * rayon ** 2
+const calculArea = (rayon) => Math.PI * rayon ** 2
 
-const calculVolume = (rayon) => 4 / 3 * (3.14 * rayon ** 3)
+const calculVolume = (rayon) => 4 / 3 * (Math.PI * rayon ** 3)
 
 const calculAireRectangle = (longueur, largeur) => longueur * largeur
