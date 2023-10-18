00ff9b2c97ffd979280299d2c8b75a2e9db7843b
rachidWassati <rachid.jeffali@wassati.com>
2023-10-18 10:40:16 +0200

    ajout calculer aire rectangle

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 const calculArea = (rayon) => Math.PI * rayon ** 2
 
 const calculVolume = (rayon) => 4 / 3 * (Math.PI * rayon ** 3)
+
+const calculRectangleArea = (longueur, largeur) => longueur + largeur
